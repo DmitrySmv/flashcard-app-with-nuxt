c63463f3dfcb6e51fcca4537a750963672577a04
Dmitry Somov <smv.dmitry@gmail.com>
2019-01-20 00:07:01 +0300

    Add Card component

---
+++ client/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='card'>
+    <div>
+      <p>{{front}}</p>
+      <nuxt-link class='btn' :to="`/deck/${deckId}/edit/${id}`">Edit</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    deckId: Number,
+    front: String
+  }
+}
+</script>
