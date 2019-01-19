34476599d30f56e2702e7441af7a4a8b78149d70
Dmitry Somov <smv.dmitry@gmail.com>
2019-01-20 00:07:26 +0300

    Display cards

--- client/pages/deck/_id.vue
+++ client/pages/deck/_id.vue
@@ -1,18 +1,38 @@
 <template>
-  <div>Deck '{{ deck.name }}'
+  <div>
+    <h1>{{deck.name}}</h1>
+    <card v-for="card of cards" v-bind="card" :key="card.id" />
     <nuxt-child />
   </div>
 </template>
 
 <script>
+import Card from '~/components/Card.vue';
+
 export default {
+  components: {
+    Card
+  },
   computed: {
+    deckId() {
+      return Number(this.$route.params.id);
+    },
     deck() {
-      const deckId = Number(this.$route.params.id);
       const decks = this.$store.state.decks.list;
 
-      return decks.find(deck => deck.id === deckId);
+      return decks.find(deck => deck.id === this.deckId);
+    },
+    cards() {
+      const cards = this.$store.state.cards.list;
+
+      return cards.filter(card => card.deckId === this.deckId);
     }
   }
 }
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
