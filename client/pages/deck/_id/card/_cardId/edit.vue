57eb505dbbd24bfa1548ebd7532fa72619329e10
Dmitry Somov <smv.dmitry@gmail.com>
2019-01-20 01:23:50 +0300

    Add Edit Card page

---
+++ client/pages/deck/_id/card/_cardId/edit.vue
@@ -0,0 +1,24 @@
+<template>
+  <card-modal header="Edit Card" :deck-id="deckId" :card="card"/>
+</template>
+
+<script>
+import CardModal from "~/components/CardModal.vue";
+
+export default {
+  components: {
+    CardModal
+  },
+  computed: {
+    deckId() {
+      return Number(this.$route.params.id);
+    },
+    cardId() {
+      return Number(this.$route.params.cardId);
+    },
+    card() {
+      return this.$store.state.cards.list.find(card => card.id === this.cardId);
+    }
+  }
+};
+</script>
--- client/store/cards.js
+++ client/store/cards.js
@@ -9,21 +9,40 @@ const mockCards = [
 
 export const state = () => ({
   list: mockCards
-})
+});
 
 export const mutations = {
-  add (state, { deckId, front, back }) {
+  add(state, { deckId, front, back }) {
     state.list.push({
       id: state.list.length + 1,
       deckId: Number(deckId),
       front,
       back
-    })
+    });
+  },
+  update(state, { id, deckId, front, back }) {
+    state.list = state.list.map(card => {
+      if (card.id === id && card.deckId === deckId) {
+        card.front = front;
+        card.back = back;
+      }
+
+      return card;
+    });
+  },
+  delete(state, { deckId, id }) {
+    state.list = state.list.filter(card => card.id !== id || card.deckId !== deckId );
   }
-}
+};
 
 export const actions = {
-  add ({ commit }, data) {
-    commit('add', data)
+  add({ commit }, data) {
+    commit('add', data);
+  },
+  update({ commit }, data) {
+    commit('update', data);
+  },
+  delete({ commit }, data) {
+    commit('delete', data);
   }
-}
+};
